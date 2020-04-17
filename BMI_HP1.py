66c816e6b60e0473ca36c469c2eb82cfe24ae45a
Hans Perez Rubin de Celis <60121637+hansperu@users.noreply.github.com>
2020-04-18 00:58:27 +0100

    Update BMI_HP1.py

--- BMI_HP1.py
+++ BMI_HP1.py
@@ -1,11 +1,15 @@
-"""Write a program that calculates somebody's Body Mass Index (BMI). The inputs are the person's height in centimetres and weight in kilograms. The output is their weight divided by their height in metres squared."""
+"""EThe objective of this exercise is to calculate someone's body mass index (BMI) (in this case I set an example by giving my name" hans "). The inputs are the person's height in centimeters and weight in kilograms The output is its weight divided by its height in square meters."""
 
 print("hans BMI")
 
-weight = float(input("Enter hans weight:"))
+# Enter my current weight 98 Kg
+weight = float(input("Enter hans weight:")) 
+
+# Height of 1.80 cm is entered
 height = float(input("Enter heigh:"))
 
 BMI = weight/(height * height)
 
+# Should result 30.24691358024691
 print ("hans, your super BMI is", BMI)
 
